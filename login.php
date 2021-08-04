6d209415eedcbe3cd5a0f6e48b8a82c5f2661bcc
Calvin-C-M <montolalu28@gmail.com>
2021-08-04 15:31:55 +0700

    Struktur login page

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+    <body>
+        <img src="" alt="Logo gopher">
+        <main>
+            <h3>Login</h3>
+            <form action="" method="post">
+                <label for="npm">NPM:
+                    <input name="npm" type="text">
+                </label>
+                <br>
+                <label for="password">Password:
+                    <input name="password" type="password">
+                </label>
+                <br>
+                <button name="login" type="submit">Login</button>
+            </form>
+        </main>
+    </body>
+</html>
