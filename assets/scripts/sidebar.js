0e873f5263082020680fa73a35327e5dde5ddc83
Calvin-C-M <montolalu28@gmail.com>
2021-08-04 15:38:32 +0700

    sidebar on dashboard

---
+++ assets/scripts/sidebar.js
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+    return (`
+    <nav>
+        <img src="" alt="Logo gopher">
+        <div id="links">
+            <a href="profile.php">Profile</a>
+            <a href="calendar.php">Timeline</a>
+            <a href="todolist.php">To Do List</a>
+        </div>
+        <a href="#">Logout</a>
+    </nav>
+    `);
+}
---
+++ user_page/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>User Dashboard</title>
+
+        <!-- Buat nanti2, bikin si sidebar buat tiap halaman kayak begini -->
+        <script src="../assets/scripts/sidebar.js"></script>
+    </head>
+    <body>
+        <script>
+            document.writeln(Sidebar);
+        </script>
+        <main>
+            
+        </main>
+    </body>
+</html>
