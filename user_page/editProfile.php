adea6ec85bf99092394634849be8873fb4f3da5f
Calvin-C-M <montolalu28@gmail.com>
2021-08-04 17:54:29 +0700

    Struktur Edit Profile

---
+++ user_page/editProfile.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edit Profile { nama }</title>
+
+        <script src="../assets/scripts/sidebar.js"></script>
+    </head>
+    <body>
+        <script>
+            document.writeln(OpenSidebar)
+            document.writeln(Sidebar)
+        </script>
+        <main>
+            <h1>Edit Profile</h1>
+            <div id="options">
+                <button id="open-data">Data</button>
+                <button id="open-password">Password</button>
+            </div>
+            <div id="edit-data" style="display:block">
+                <form action="" method="post">
+                    <label for="nama">Nama
+                        <input name="nama" type="text">
+                    </label>
+                    <br>
+                    <label for="npm">NPM
+                        <input nama="npm" type="text">
+                    </label>
+                    <br>
+                    <label for="kelas">Kelas
+                        <input nama="kelas" type="text">
+                    </label>
+                    <br>
+                    <label for="tgl_lahir">Tanggal Lahir
+                        <input name="tgl_lahir" type="date">
+                    </label>
+                    <br>
+                    <label for="alamat">Alamat
+                        <textarea name="alamat" cols="30" rows="10"></textarea>
+                    </label>
+                    <br>
+                    <button name="save" type="submit">Save</button>
+                </form>
+            </div>
+            <div id="edit-password" style="display:none">
+                <form action="" method="post">
+                    <label for="old_pw">Old Password:
+                        <input name="old_pw" type="password">
+                    </label>
+                    <br>
+                    <label for="new_pw">New Password:
+                        <input name="new_pw" type="password">
+                    </label>
+                    <br>
+                    <label for="confirm_pw">Confirm New Password:
+                        <input name="confirm_pw" type="password">
+                    </label>
+                    <br>
+                    <button name="save" type="submit">Save</button>
+                </form>
+            </div>
+        </main>
+    </body>
+</html>
